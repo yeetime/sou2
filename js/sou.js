ff2c680407212db739c87e783f18a8912870859a
yizhou <yeetime@qq.com>
2020-01-11 14:09:11 +0800

    动态加载默认搜索引擎

--- js/sou.js
+++ js/sou.js
@@ -19,13 +19,14 @@ $(document).ready(function() {
         $(".wd").focus();
     }
 
-    //选择搜索引擎世界
+    //选择搜索引擎
     $(document).on('click',function(e){
         var con = $(".search-engine");
         var img = $(".se");
 
         if($(".search-engine").is(":hidden") && img.is(e.target)){
             if (img.is(e.target)) {
+                seList();
                 $(".search-engine").show();
             }
         }else{
@@ -36,10 +37,10 @@ $(document).ready(function() {
     });
 
     //搜索引擎列表点击
-    $(".se-li").click(function(){
-        url = $(this).attr('url');
-        name = $(this).attr('name');
-        img = $(this).attr('img')
+    $(".search-engine-list").on("click",".se-li",function(){
+        var url = $(this).attr('url');
+        var name = $(this).attr('name');
+        var img = $(this).attr('img');
         $("#search").attr("action",url);
         $(".wd").attr("name",name);
         $(".se").attr("src",img);
@@ -56,9 +57,10 @@ $(document).ready(function() {
         $(".list").addClass('closed');
     });
 
-    //菜单展开动画
+    //设置展开动画
     $(".set").click(function() {
         if($("#controll").is(":hidden")){
+            setinit();
             $("#controll").show();
             $("#controll")[0].style.height = 200+"px";
             $(".set").html("关闭");
@@ -71,33 +73,83 @@ $(document).ready(function() {
 
     //设置默认搜索引擎
     $(".se_list").on("click",".se_l",function(){
-
         var name = $(this).val();
         Cookies.set('se_default', name, { expires: 30 });
-        alert("默认搜索引擎已保存");
+        //alert("默认搜索引擎已保存");
     });
 
     //初始化
     var se_list = {
-        1:{
+        'baidu':{
             id      :1,
             title   :"百度",
             url     :"https://www.baidu.com/s",
             name    :"wd",
             img     :"./icon/baidu.ico",
         },
-        2:{
+        'google':{
             id      :2,
             title   :"谷歌",
             url     :"https://www.google.com/search",
             name    :"q",
             img     :"./icon/google_1.png",
         },
+        'bing':{
+            id      :3,
+            title   :"必应",
+            url     :"https://cn.bing.com/search",
+            name    :"q",
+            img     :"./icon/bing.ico",
+        },
+        'dogedoge':{
+            id      :4,
+            title   :"多吉",
+            url     :"https://www.dogedoge.com/results",
+            name    :"q",
+            img     :"./icon/doge_ico.png",
+        },
+        'mijisou':{
+            id      :5,
+            title   :"秘迹",
+            url     :"https://mijisou.com",
+            name    :"q",
+            img     :"./icon/mijisou.png",
+        },
+        'seeres':{
+            id      :6,
+            title   :"seeres*",
+            url     :"https://seeres.com/search",
+            name    :"q",
+            img     :"./icon/seeres.png",
+        },
     };
 
     var se_default = Cookies.get('se_default');
+    //首页数据加载
+    homeData();
 
-    setinit();
+    //默认搜索引擎加载
+    function homeData() {
+        var defaultSe = se_list[se_default];
+        $("#search").attr("action", defaultSe["url"]);
+        $(".se").attr("src", defaultSe["img"]);
+        $(".wd").attr("name", defaultSe["name"]);
+    }
+    //搜索引擎列表加载
+    function seList() {
+        var html = "";
+        for(var i in se_list){
+            html+="<li class='se-li' url='"+se_list[i]["url"]+"' name='"+se_list[i]["name"]+"' img='"+se_list[i]["img"]+"'><img src='"+se_list[i]["img"]+"'></img>"+se_list[i]["title"]+"</li>"
+
+            // if(i == se_default){
+            //     html+="<input type='radio' class='se_l' name='se_default' value='"+i+"' checked='checked'> "+ se_list[i]["title"];
+            // } else {
+            //     html+="<input type='radio' class='se_l' name='se_default' value='"+i+"'> "+ se_list[i]["title"];
+            // }
+        }
+        $(".search-engine-list").html(html);
+    }
+    //设置内容加载
     function setinit () {
         var html = "";
         for(var i in se_list){
